cef188220b3bc8222e188448672f238f08dbce4c
Ethan Lowman <ethan.l.527@gmail.com>
2014-04-23 02:19:56 -0400

    Show ip address

--- svddemo.py
+++ svddemo.py
@@ -2,7 +2,9 @@ import os
 import numpy
 import pymongo
 
-from flask import Flask
+from flask import (Flask,
+                   request,
+                   render_template)
 
 app = Flask(__name__)
 
@@ -10,8 +12,8 @@ app = Flask(__name__)
 @app.route("/")
 def demo():
     con = pymongo.Connection(os.getenv("MONGOHQ_URL"))
-    return "Hello world!"
+    return render_template("select_favorites.html", ip=request.remote_addr)
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
