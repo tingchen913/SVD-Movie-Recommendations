6b4c667f0306956a55fb7249028d8bd85e74678e
Ethan Lowman <ethan.l.527@gmail.com>
2014-05-05 22:47:45 -0400

    Push on deploy

--- fabfile.py
+++ fabfile.py
@@ -2,6 +2,7 @@ from __future__ import with_statement
 from fabric.api import *
 
 def deploy():
+    local("git push")
     with cd("/usr/local/www/svddemo"):
         run("git pull")
         run("npm install")
